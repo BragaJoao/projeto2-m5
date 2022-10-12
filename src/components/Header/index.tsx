a474b49f83bde1059cf26c984454cf2445246244
Caio Rodrigo Silva <78828999+Caio-Rodrigo@users.noreply.github.com>
2022-10-11 23:19:56 -0300

    feat: 20% page usuarios

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import './header.scss'
+// import { CiLogout } from "react-icons/ci";
+
+export default function Header (){
+  return(
+    <header>
+      <div>
+        <div className="logo">
+          <h1>GameFlix</h1>
+        </div>
+        <div className="sair">
+       
+        </div>
+      </div>
+    </header>
+  )
+} 
